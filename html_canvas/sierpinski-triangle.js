68bd433a81f51ccd6b5ce45c4ad9891704740a59
Dejan Todorovic <deja99@live.com>
2022-03-06 02:46:28 +0100

    feat: Triangle recirsove cutting

--- html_canvas/sierpinski-triangle.js
+++ html_canvas/sierpinski-triangle.js
@@ -11,19 +11,65 @@ class Sierpinski {
         if (this.canvas.getContext) this.ctx = this.canvas.getContext("2d");
         else alert("Canvas error")
 
+        this.triangles = [];
+
         document.getElementById("start").addEventListener('click', this.startCanvas.bind(this));
+        document.getElementById("next_iter").addEventListener('click', this.nextTrinagleCut.bind(this));
+    }
+
+    resizeCanvas() {
+        this.canvas.width = document.getElementById('inputWidth').value
+        this.canvas.height = document.getElementById('inputHeight').value
     }
 
     startCanvas() {
-        this.drawTriangle([10,100],50);
+        this.resizeCanvas();
+
+        let length = Math.min(this.canvas.width, this.canvas.height)
+
+        this.drawTriangle([0,0,length], false);
+        this.triangles.push([0,0,length]);
     }
 
-    drawTriangle(start, length) {
+    getTriangleHeight (length) {
+        return length * Math.sqrt(3) / 2
+    }
+
+    drawTriangle(triangle, upsideDown=false, fill="red", topLeft=true) {
+        let [x,y,length] = triangle;
+        let direction = upsideDown ? -1 : 1;
+        if (topLeft && !upsideDown) y += this.getTriangleHeight(length); 
+
         this.ctx.beginPath();
-        this.ctx.moveTo(start[0], start[1]);
-        this.ctx.lineTo(start[0] + length, start[1]);
-        this.ctx.lineTo(start[0] + length/2, length);
+        this.ctx.moveTo(x, y);
+        this.ctx.lineTo(x + length, y);
+        this.ctx.lineTo(x + length/2, y - (this.getTriangleHeight(length) * direction ));
+        this.ctx.fillStyle = fill;
         this.ctx.fill();
+    }
+
+
+    addSmallTriangles(x, y, length) {
+        let height = this.getTriangleHeight(length);
+        let triangle1 = [x + length/4     , y               , length/2];
+        let triangle2 = [x                , y + height / 2  , length/2];
+        let triangle3 = [x + length / 2   , y + height / 2  , length/2];
+        this.triangles.push(triangle1);
+        this.triangles.push(triangle2);
+        this.triangles.push(triangle3);
+    }
+
+    nextTrinagleCut() {
+        let [x,y,length] = this.triangles.pop();
+        
+        let white_y = y + this.getTriangleHeight(length)/2;
+        let white_x = x + length / 4;
+
+        this.addSmallTriangles(x,y,length);
+        this.drawTriangle([white_x, white_y, length/2], true, "white");
+
+
+
     }
 
     test(params) {
